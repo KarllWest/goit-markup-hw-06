81881241b19ac0d46273e087cc43442aeb802b6b
KarllWest <karlwest396@gmail.com>
2025-08-21 17:33:58 +0100

    Adaptive design fix

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,13 @@
 (() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-menu]"),
-  };
+  const menu = document.querySelector('[data-menu]');
+  const openBtn = document.querySelector('[data-menu-open]');
+  const closeBtn = document.querySelector('[data-menu-close]');
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  openBtn.addEventListener('click', () => {
+    menu.classList.add('is-open');
+  });
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
-  }
+  closeBtn.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+  });
 })();
